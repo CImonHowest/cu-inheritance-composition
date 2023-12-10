5e234d32e7b4258dd00f123c5c893f84ced1ced6
Simon Soenens <simon.soenens@student.howest.be>
2023-12-10 12:58:19 +0100

    Add classes

---
+++ Pre.Animal.Comopsition.Core/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pre.Animal.Comopsition.Core
+{
+    public class Animal
+    {
+        public string Color { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+}
