0c0a3b8bd398dcdd22352f53bde0f5d3ae907821
Simon Soenens <simon.soenens@student.howest.be>
2023-12-10 13:02:52 +0100

    Add classes ANimal dog and Lion

--- Pre.Animal.Comopsition.Core/Dog.cs
+++ Pre.Animal.Comopsition.Core/Dog.cs
@@ -8,6 +8,28 @@ namespace Pre.Animal.Comopsition.Core
 {
     public class Dog
     {
-        public string Dog.Color { get; set; }
+        private Animal _animal;
+
+        public string Color
+        {
+            get { return _animal.Color; }
+            set { _animal.Color = value; }
+        }
+
+        public Dog()
+        {
+            _animal = new Animal();
+        }
+
+        public void Eat()
+        {
+            _animal.Eat();
+            Console.WriteLine("...like a dog");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woof!");
+        }   
     }
 }
--- Pre.Animal.Comopsition.Core/Lion.cs
+++ Pre.Animal.Comopsition.Core/Lion.cs
@@ -8,5 +8,30 @@ namespace Pre.Animal.Comopsition.Core
 {
     public class Lion
     {
+        public string JungleName { get; set; }
+
+        private Animal _animal;
+
+        public string Color
+        {
+            get { return _animal.Color; }
+            set { _animal.Color = value; }
+        }
+
+        public Lion()
+        {
+            _animal = new Animal();
+        }
+
+        public void Eat()
+        {
+            _animal.Eat();
+            Console.WriteLine("...like a lion");
+        }
+
+        public void Roar()
+        {
+            Console.WriteLine("Roar!");
+        }   
     }
 }
